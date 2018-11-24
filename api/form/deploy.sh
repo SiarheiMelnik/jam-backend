a444342c0096ab3dbf5d36fb06a1635a0b54ac9b
siarhei melnik <siarhei.m.a@gmail.com>
2018-11-24 19:14:12 +0300

    feat: browser

--- api/form/deploy.sh
+++ api/form/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sam deploy \
     --template-file packaged.yaml \
-    --stack-name jam-be  \
+    --stack-name jam-be-form  \
     --capabilities CAPABILITY_IAM \
     --region us-east-1
