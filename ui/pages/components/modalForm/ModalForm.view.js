d0efe2408bb0c81388fd0d2fbd9c0eaaa315b83d
Aliaksandr Rudzin <Aliaksandr_Rudzin@epam.com>
2018-11-24 15:27:01 +0300

    Add modal component

---
+++ ui/pages/components/modalForm/ModalForm.view.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+import { Modal, Button } from 'react-bootstrap';
+
+export class ModalForm extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+    this.state = {
+      show: false,
+    };
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  render() {
+    return (
+      <div className="modal-form">
+        <Button variant="primary" onClick={this.handleShow}>
+          Open form
+        </Button>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
--- ui/pages/index.js
+++ ui/pages/index.js
@@ -1,3 +1,18 @@
-import TreatmentView from './components/treeatment';
+import './index.css'; 
+import { Button } from 'react-bootstrap';
+import TreatmentView from './components/treatment';
+import ModalForm from './components/modalForm';
 
-export default () => <TreatmentView />;
+export default () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div class="col-sm">
+          <ModalForm /> 
+        </div>
+      </div>  
+      
+      {/* <TreatmentView /> */}
+    </div>
+  );
+}
