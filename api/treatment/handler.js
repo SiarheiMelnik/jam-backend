28adec00402bbfc2b816fd787967b9d4e5fda3d8
siarhei melnik <siarhei.m.a@gmail.com>
2018-11-24 12:56:50 +0300

    feat: add request

--- api/treatment/handler.js
+++ api/treatment/handler.js
@@ -8,16 +8,20 @@ const CORS = {
     'Access-Control-Allow-Methods': '*'
 };
 
-exports.treatment = async (event) => {
-    console.log(FEEDBACK_URL);
-    
+exports.treatment = async (evt) => {
+    let response;
     try {
+        console.log(FEEDBACK_URL);
+        console.log(evt.body);
+
+        const data = JSON.parse(evt.body || '{}');
+        
+        console.log(data);
+
         response = {
             'statusCode': 200,
             headers: { ...CORS },
-            'body': JSON.stringify({
-                message: 'hi',
-            })
+            'body': JSON.stringify({ form: data, status: 'OK' })
         }
     } catch (err) {
         console.log(err);
